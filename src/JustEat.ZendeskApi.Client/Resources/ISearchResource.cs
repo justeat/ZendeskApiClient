7b724505ce5d45ec4dae1bdf054024292b995fb5
Hjalte Thor <hjalte.thor@just-eat.com>
2014-07-10 13:56:02 +0100

    BAPPS-1644: Add FindAll to SearchResource

--- src/JustEat.ZendeskApi.Client/Resources/ISearchResource.cs
+++ src/JustEat.ZendeskApi.Client/Resources/ISearchResource.cs
@@ -7,5 +7,6 @@ namespace JustEat.ZendeskApi.Client.Resources
     public interface ISearchResource
     {
         IListResponse<T> Find<T>(IZendeskQuery<T> zendeskQuery) where T : IZendeskEntity;
+        IListResponse<T> FindAll<T>() where T : IZendeskEntity;
     }
 }
