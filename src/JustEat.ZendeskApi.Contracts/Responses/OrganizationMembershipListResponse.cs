2c5399943fcb74f68aacd30cda9774a71431e5a7
Hjalte Thor <hjalte.thor@just-eat.com>
2014-07-15 15:51:29 +0100

    BAPPS-1644: Add OrganizationMembership to Zendesk API

---
+++ src/JustEat.ZendeskApi.Contracts/Responses/OrganizationMembershipListResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using JustEat.ZendeskApi.Contracts.Models;
+
+namespace JustEat.ZendeskApi.Contracts.Responses
+{
+    [DataContract]
+    internal class OrganizationMembershipListResponse : ListResponse<OrganizationMembership>
+    {
+        [DataMember(Name = "organization_memberships")]
+        public override IEnumerable<OrganizationMembership> Results { get; set; }
+    }
+}
