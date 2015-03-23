b1fbf59bfbfdf0647b648cb275c9dc32fe1c4763
kate <kate.richardson@just-eat.com>
2015-03-23 11:42:41 +0000

    Removing unused test code

--- src/ZendeskApi.Acceptance/RequestSteps.cs
+++ src/ZendeskApi.Acceptance/RequestSteps.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web;
 using ZendeskApi.Client;
 using ZendeskApi.Contracts.Models;
 using ZendeskApi.Contracts.Requests;
@@ -18,9 +17,6 @@ namespace ZendeskApi.Acceptance
         private IZendeskClient _client;
 
         private readonly List<Request> _savedMultipleRequest = new List<Request>();
-        private readonly List<Request> _multipleRequestResponse = new List<Request>();
-
-        private int _customFieldId;
 
         private Request _savedSingleRequest;
         private Request _singleRequestResponse;
@@ -32,16 +28,6 @@ namespace ZendeskApi.Acceptance
                 new ZendeskDefaultConfiguration(ConfigurationManager.AppSettings["zendeskusername"], ConfigurationManager.AppSettings["zendesktoken"]));
         }
 
-        [Given(@"the following requests in Zendesk")]
-        [Given(@"I post the following requests")]
-        public void GivenTheFollowingRequestsInZendesk(Table table)
-        {
-            var requests = table.Rows.Select(row => new Request { Subject = row["Subject"], Comment = new TicketComment { Body = row["Description"] }}).ToList();
-
-            requests.ForEach(t => _savedMultipleRequest.Add(_client.Requests.Post(new RequestRequest { Item = t }).Item));
-        }
-
-
         [Given(@"a request in Zendesk with the subject '(.*)' and comment '(.*)'")]
         public void GivenARequestInZendeskWithTheSubjectAndDescriptionTWorkInTheseConditions(string subject, string comment)
         {
@@ -49,17 +35,6 @@ namespace ZendeskApi.Acceptance
                 _client.Requests.Post(new RequestRequest
                 {
                     Item = new Request { Subject = subject, Type = TicketType.task, Comment = new TicketComment { Body = comment } }
-                }).Item;
-        }
-
-
-        [Given(@"a request in Zendesk with the subject '(.*)' and description '(.*)' and type '(.*)'")]
-        public void GivenARequestInZendeskWithTheSubjectAndDescriptionIWorkInTheseConditions(string subject, string description, string type)
-        {
-            _savedSingleRequest =
-                _client.Requests.Post(new RequestRequest
-                {
-                    Item = new Request { Subject = subject, Comment = new TicketComment { Body = description }, Type = (TicketType)Enum.Parse(typeof(TicketType), type) }
                 }).Item;
         }
 
@@ -85,37 +60,6 @@ namespace ZendeskApi.Acceptance
         public void ThenIGetARequestViaTheApi()
         {
             Assert.That(_singleRequestResponse.Via.Channel, Is.EqualTo("api"));
-        }
-
-        [When(@"I call delete by id on the ZendeskApiClient")]
-        public void WhenICallDeleteByIdOnTheZendeskApiClient()
-        {
-            _client.Requests.Delete((long)_savedSingleRequest.Id);
-        }
-
-        [Then(@"the request is no longer in zendesk")]
-        public void ThenTheRequestIsNoLongerInZendesk()
-        {
-            Assert.Throws<HttpException>(() => _client.Requests.Get((long) _savedSingleRequest.Id), "Requests not in Zendesk");
-        }
-
-        [AfterScenario]
-        public void AfterFeature()
-        {
-            try
-            {
-                if (_savedSingleRequest != null)
-                    _client.Requests.Delete((long)_savedSingleRequest.Id);
-
-                _savedMultipleRequest.ForEach(t => _client.Requests.Delete((long)t.Id));
-
-            }
-            catch (HttpException)
-            {
-                
-            }
-            
-        }
-        
+        }  
     }
 }
--- src/ZendeskApi.Acceptance/Requests.feature
+++ src/ZendeskApi.Acceptance/Requests.feature
@@ -13,11 +13,6 @@ Scenario: When I call Post I am able to add a request
 	When I call get by id on the ZendeskApiClient
 	Then I get a request from Zendesk with the subject 'I've swallowed my mouse cable' and description 'It's a bit of a problem'
 
-Scenario: When I call Delete the request is deleted from zendesk
-	Given a request in Zendesk with the subject 'The coffee machiene is broken' and comment 'I can't work in these conditions!'
-	When I call delete by id on the ZendeskApiClient
-	Then the request is no longer in zendesk
-
 Scenario: When I call Get by id, I get told it was created via the api
 	Given a request in Zendesk with the subject 'The coffee machiene is broken' and comment 'I can't work in these conditions!'
 	When I call get by id on the ZendeskApiClient
