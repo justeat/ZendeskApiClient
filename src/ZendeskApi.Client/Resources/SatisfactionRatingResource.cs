c28c3cd9a31ceeefa9a1e6d05d77952a8ce83659
Eduard <eduard.truuvaart@just-eat.com>
2016-09-29 10:08:30 +0100

    COGCC-1569 - SatisfactionRatingResource refactoring

--- src/ZendeskApi.Client/Resources/SatisfactionRatingResource.cs
+++ src/ZendeskApi.Client/Resources/SatisfactionRatingResource.cs
@@ -6,38 +6,37 @@ using ZendeskApi.Contracts.Responses;
 
 namespace ZendeskApi.Client.Resources
 {
-    public class SatisfactionRatingResource : ZendeskResource<SatisfactionRating>, ISatisfactionRatingResource
+    public class SatisfactionRatingResource : ISatisfactionRatingResource
     {
-        private string _resourceUrl;
-        protected override string ResourceUri => _resourceUrl ?? @"/api/v2/tickets/{0}/satisfaction_rating";
+        private readonly IRestClient _client;
 
         public SatisfactionRatingResource(IRestClient client)
         {
-            Client = client;
+            _client = client;
         }
 
         public IResponse<SatisfactionRating> Get(long id)
         {
-            _resourceUrl = "/api/v2/satisfaction_ratings";
-
-            return Get<SatisfactionRatingResponse>(id);
+            var requestUri = _client.BuildUri($"/api/v2/satisfaction_ratings/{id}");
+            return _client.Get<SatisfactionRatingResponse>(requestUri);
         }
 
         public async Task<IResponse<SatisfactionRating>> GetAsync(long id)
         {
-            _resourceUrl = "/api/v2/satisfaction_ratings";
-
-            return await GetAsync<SatisfactionRatingResponse>(id).ConfigureAwait(false);
+            var requestUri = _client.BuildUri($"/api/v2/satisfaction_ratings/{id}");
+            return await _client.GetAsync<SatisfactionRatingResponse>(requestUri).ConfigureAwait(false);
         }
 
         public IResponse<SatisfactionRating> Post(SatisfactionRatingRequest request, long ticketId)
         {
-            return Post<SatisfactionRatingRequest, SatisfactionRatingResponse>(request, ticketId);
+            var requestUri = _client.BuildUri($"/api/v2/tickets/{ticketId}/satisfaction_rating");
+            return _client.Post<SatisfactionRatingResponse>(requestUri, request);
         }
 
         public async Task<IResponse<SatisfactionRating>> PostAsync(SatisfactionRatingRequest request, long ticketId)
         {
-            return await PostAsync<SatisfactionRatingRequest, SatisfactionRatingResponse>(request, ticketId).ConfigureAwait(false);
+            var requestUri = _client.BuildUri($"/api/v2/tickets/{ticketId}/satisfaction_rating");
+            return await _client.PostAsync<SatisfactionRatingResponse>(requestUri, request).ConfigureAwait(false);
         }
     }
 }
