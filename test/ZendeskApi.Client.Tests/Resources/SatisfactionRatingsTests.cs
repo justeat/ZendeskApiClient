15fcd6c833a23c33d4ad38618ff411c0477b4009
Nick Mayne <Jetski5822@gmail.com>
2017-06-09 14:26:40 +0100

    adding tests

---
+++ test/ZendeskApi.Client.Tests/Resources/SatisfactionRatingsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using Xunit;
+using ZendeskApi.Client.Resources;
+using ZendeskApi.Client.Models;
+
+namespace ZendeskApi.Client.Tests.Resources
+{
+    public class SatisfactionRatingsTests : IDisposable
+    {
+        private readonly IZendeskApiClient _client;
+        private readonly SatisfactionRatingsResource _resource;
+
+        public SatisfactionRatingsTests()
+        {
+            _client = new DisposableZendeskApiClient((resource) => new SatisfactionRatingsResourceSampleSite(resource));
+            _resource = new SatisfactionRatingsResource(_client, NullLogger.Instance);
+        }
+
+        [Fact]
+        public async Task ShouldListAllSatisfactionRatings()
+        {
+            var obj1 = await _resource.CreateSatisfactionRatingAsync(new SatisfactionRating { Url = "http://fu.com" }, 1);
+            var obj2 = await _resource.CreateSatisfactionRatingAsync(new SatisfactionRating { Url = "http://fu2.com" }, 1);
+
+            var objs = (await _resource.GetAllAsync()).ToArray();
+
+            Assert.Equal(2, objs.Length);
+            Assert.Equal(JsonConvert.SerializeObject(obj1), JsonConvert.SerializeObject(objs[0]));
+            Assert.Equal(JsonConvert.SerializeObject(obj2), JsonConvert.SerializeObject(objs[1]));
+        }
+        
+        [Fact]
+        public async Task ShouldGetSatisfactionRating()
+        {
+            var obj1 = await _resource.CreateSatisfactionRatingAsync(new SatisfactionRating { Url = "http://fu.com" }, 1);
+            var obj2 = await _resource.CreateSatisfactionRatingAsync(new SatisfactionRating { Url = "http://fu2.com" }, 1);
+
+            var response = await _resource.GetAsync(obj1.Id.Value);
+
+            Assert.Equal(JsonConvert.SerializeObject(obj1), JsonConvert.SerializeObject(response));
+        }
+        
+        [Fact]
+        public async Task ShouldCreateTicket()
+        {
+            var obj1 = await _resource.CreateSatisfactionRatingAsync(new SatisfactionRating { Url = "http://fu.com" }, 1);
+
+            Assert.NotNull(obj1.Id);
+            Assert.Equal("http://fu.com", obj1.Url);
+        }
+        
+        public void Dispose()
+        {
+            ((IDisposable)_client).Dispose();
+        }
+    }
+}
---
+++ test/ZendeskApi.Client.Tests/ResourcesSampleSites/SatisfactionRatingsResourceSampleSite.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using ZendeskApi.Client.Models;
+using ZendeskApi.Client.Requests;
+using ZendeskApi.Client.Responses;
+using ZendeskApi.Client.Tests.ResourcesSampleSites;
+
+namespace ZendeskApi.Client.Tests
+{
+    public class SatisfactionRatingsResourceSampleSite : SampleSite
+    {
+        private class State
+        {
+            public IDictionary<long, SatisfactionRating> SatisfactionRatings = new Dictionary<long, SatisfactionRating>();
+            public IDictionary<long, List<SatisfactionRating>> SatisfactionRatingsByTicket = new Dictionary<long, List<SatisfactionRating>>();
+        }
+
+        public static Action<IRouteBuilder> MatchesRequest
+        {
+            get
+            {
+                return rb => rb
+                    .MapGet("api/v2/satisfaction_ratings/{id}", (req, resp, routeData) =>
+                    {
+                        var id = long.Parse(routeData.Values["id"].ToString());
+
+                        var state = req.HttpContext.RequestServices.GetRequiredService<State>();
+
+                        if (!state.SatisfactionRatings.ContainsKey(id))
+                        {
+                            resp.StatusCode = (int)HttpStatusCode.NotFound;
+                            return Task.CompletedTask;
+                        }
+
+                        var sr = state.SatisfactionRatings.Single(x => x.Key == id).Value;
+
+                        resp.StatusCode = (int)HttpStatusCode.OK;
+                        return resp.WriteAsync(JsonConvert.SerializeObject(new SatisfactionRatingResponse { Item = sr }));
+                    })
+                    .MapGet("api/v2/satisfaction_ratings", (req, resp, routeData) =>
+                    {
+                        var state = req.HttpContext.RequestServices.GetRequiredService<State>();
+
+                        resp.StatusCode = (int)HttpStatusCode.OK;
+                        return resp.WriteAsync(JsonConvert.SerializeObject(new SatisfactionRatingsResponse { Item = state.SatisfactionRatings.Values }));
+                    })
+                    .MapPost("api/v2/tickets/{ticketId}/satisfaction_rating", (req, resp, routeData) =>
+                    {
+                        var sr = req.Body.Deserialize<SatisfactionRatingRequest>().Item;
+                        var ticketId = long.Parse(routeData.Values["ticketId"].ToString());
+
+                        if (sr.Comment != null && sr.Comment.Body.Contains("error"))
+                        {
+                            resp.StatusCode = (int)HttpStatusCode.PaymentRequired; // It doesnt matter as long as not 201
+
+                            return Task.CompletedTask;
+                        }
+
+                        var state = req.HttpContext.RequestServices.GetRequiredService<State>();
+
+                        sr.Id = long.Parse(RAND.Next().ToString());
+                        state.SatisfactionRatings.Add(sr.Id.Value, sr);
+
+                        if (state.SatisfactionRatingsByTicket.ContainsKey(ticketId))
+                        {
+                            state.SatisfactionRatingsByTicket[ticketId].Add(sr);
+                        }
+                        else {
+                            state.SatisfactionRatingsByTicket.Add(ticketId, new List<SatisfactionRating> { sr });
+                        }
+
+                        resp.StatusCode = (int)HttpStatusCode.Created;
+                        return resp.WriteAsync(JsonConvert.SerializeObject(new SatisfactionRatingResponse { Item = sr }));
+                    })
+                    ;
+            }
+        }
+
+        private readonly TestServer _server;
+
+        private HttpClient _client;
+        public override HttpClient Client => _client;
+        
+        public SatisfactionRatingsResourceSampleSite(string resource)
+        {
+            var webhostbuilder = new WebHostBuilder();
+            webhostbuilder
+                .ConfigureServices(services => {
+                    services.AddSingleton<State>((_) => new State());
+                    services.AddRouting();
+                    services.AddMemoryCache();
+                })
+                .Configure(app =>
+                {
+
+                    app.UseRouter(MatchesRequest);
+                });
+
+            _server = new TestServer(webhostbuilder);
+            
+            RefreshClient(resource);
+        }
+
+        public override void RefreshClient(string resource)
+        {
+            _client = _server.CreateClient();
+            _client.BaseAddress = new Uri($"http://localhost/{CreateResource(resource)}");
+        }
+
+        private string CreateResource(string resource)
+        {
+            resource = resource?.Trim('/');
+
+            return resource != null ? resource + "/" : resource;
+        }
+
+        public Uri BaseUri
+        {
+            get { return Client.BaseAddress; }
+        }
+
+        public override void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
