ab4811a934c1347e23a9c1429184871920c509b8
Nicholas Mayne <Jetski5822@gmail.com>
2017-06-26 22:35:59 +0100

    making sure Ticket class follows spec and removign json ignores from it

---
+++ src/ZendeskApi.Client.Abstractions/Models/Priority.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace ZendeskApi.Client.Models
+{
+    public enum Priority
+    {
+        [EnumMember(Value = "urgent")]
+        Urgent,
+        [EnumMember(Value = "high")]
+        High,
+        [EnumMember(Value = "normal")]
+        Normal,
+        [EnumMember(Value = "low")]
+        Low
+    }
+}
--- src/ZendeskApi.Client.Abstractions/Models/Ticket.cs
+++ src/ZendeskApi.Client.Abstractions/Models/Ticket.cs
@@ -12,14 +12,11 @@ namespace ZendeskApi.Client.Models
         [JsonProperty]
         public long? Id { get; set; }
 
-        [JsonProperty("created_at")]
-        public DateTime? CreatedAt { get; set; }
+        [JsonProperty]
+        public Uri Url { get; set; }
 
-        [JsonProperty("updated_at")]
-        public DateTime? UpdatedAt { get; set; }
-
-        [JsonProperty("due_at")]
-        public DateTime? Due { get; set; }
+        [JsonProperty("external_id")]
+        public string ExternalId { get; set; }
 
         [JsonConverter(typeof(StringEnumConverter))]
         [JsonProperty("type")]
@@ -28,15 +25,23 @@ namespace ZendeskApi.Client.Models
         [JsonProperty("subject")]
         public string Subject { get; set; }
 
+        [JsonProperty("raw_subject")]
+        public string RawSubject { get; set; }
+
         [JsonProperty("description")]
         public string Description { get; set; }
 
-        [JsonProperty("comment")]
-        public TicketComment Comment { get; set; }
+        // TODO: Enum
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty("priority")]
+        public Priority Priority { get; set; }
 
         [JsonConverter(typeof(StringEnumConverter))]
         [JsonProperty("status")]
         public TicketStatus Status { get; set; }
+
+        [JsonProperty("recipient")]
+        public object Recipient { get; set; }
 
         [JsonProperty("requester_id")]
         public long? RequesterId { get; set; }
@@ -50,20 +55,26 @@ namespace ZendeskApi.Client.Models
         [JsonProperty("organization_id")]
         public long? OrganisationId { get; set; }
 
-        [JsonProperty]
-        public Uri Url { get; set; }
-
-        [JsonProperty("priority")]
-        public string Priority { get; set; }
-
-        [JsonProperty("recipient")]
-        public object Recipient { get; set; }
-
         [JsonProperty("group_id")]
         public long? GroupId { get; set; }
 
-        [JsonProperty("ticket_form_id")]
-        public int? FormId { get; set; }
+        [JsonProperty("collaborator_ids")]
+        public List<long> CollaboratorIds { get; set; }
+
+        [JsonProperty("forum_topic_id")]
+        public long? ForumTopicId { get; set; }
+
+        [JsonProperty("problem_id")]
+        public long? ProblemId { get; set; }
+
+        [JsonProperty("has_incidents")]
+        public bool HasIncidents { get; set; }
+
+        [JsonProperty("due_at")]
+        public DateTime? Due { get; set; }
+
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
 
         [JsonProperty("via")]
         public Via Via { get; set; }
@@ -72,47 +83,41 @@ namespace ZendeskApi.Client.Models
         public List<CustomField> CustomFields { get; set; }
 
         [JsonProperty("satisfaction_rating")]
-        public object SatisfactionRating { get; set; }
-        
-        [JsonProperty("external_id")]
-        public string ExternalId { get; set; }
-        
-        [JsonProperty("collaborator_ids")]
-        public List<long> CollaboratorIds { get; set; }
+        public SatisfactionRating SatisfactionRating { get; set; }
 
-        [JsonIgnore]
-        [JsonProperty("forum_topic_id")]
-        public long ForumTopicId { get; set; }
-
-        [JsonIgnore]
-        [JsonProperty("problem_id")]
-        public long ProblemId { get; set; }
-
-        [JsonIgnore]
-        [JsonProperty("has_incidents")]
-        public bool HasIncidents { get; set; }
-
-        [JsonProperty("tags")]
-        public List<string> Tags { get; set; }
-
-        [JsonIgnore]
         [JsonProperty("sharing_agreement_ids")]
-        public List<object> SharingAgreementIds { get; set; }
+        public List<long> SharingAgreementIds { get; set; }
 
-        [JsonIgnore]
-        [JsonProperty("fields")]
-        public List<object> Fields { get; set; }
-
-        [JsonIgnore]
-        [JsonProperty("result_type")]
-        public string ResultType { get; set; }
-
-        [JsonIgnore]
         [JsonProperty("followup_ids")]
         public List<long> FollowupIds { get; set; }
 
+        [JsonProperty("ticket_form_id")]
+        public int? FormId { get; set; }
+
+        [JsonProperty("brand_id")]
+        public int? BrandId { get; set; }
+
+        [JsonProperty("allow_channelback")]
+        public bool AllowChannelback { get; set; }
+
+        [JsonProperty("is_public")]
+        public bool IsPublic { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        
+        // Requred for Create
+        [JsonProperty("comment")]
+        public TicketComment Comment { get; set; }
+
+        // Requred for Create
         [JsonProperty("requester")]
         public TicketRequester Requester { get; set; }
+
 
         DateTime ISearchResult.CreatedAt => CreatedAt.Value;
         DateTime ISearchResult.UpdatedAt => UpdatedAt.Value;
