ffe20a111637b1e8b875874fa9675cf21c202df2
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-03-12 11:31:33 +0000

    add skeleton integration tests project

---
+++ ZendeskApi.Client.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ZendeskApi.Client.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
