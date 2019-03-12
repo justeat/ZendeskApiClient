7d2d355edee6ecfe4deec84f9ef728b9660257e1
Jonathan Stowell <stowell.jonathan@gmail.com>
2019-03-12 18:55:01 +0000

    log url on int test run error

--- test/ZendeskApi.Client.IntegrationTests/Resources/OrganizationResourceTests.cs
+++ test/ZendeskApi.Client.IntegrationTests/Resources/OrganizationResourceTests.cs
@@ -2,23 +2,33 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 using ZendeskApi.Client.IntegrationTests.Factories;
+using ZendeskApi.Client.IntegrationTests.Settings;
 using ZendeskApi.Client.Models;
 
 namespace ZendeskApi.Client.IntegrationTests.Resources
 {
     public class OrganizationResourceTests : IClassFixture<ZendeskClientFactory>
     {
+        private readonly ITestOutputHelper _output;
         private readonly ZendeskClientFactory _clientFactory;
 
-        public OrganizationResourceTests(ZendeskClientFactory clientFactory)
+        public OrganizationResourceTests(
+            ITestOutputHelper output,
+            ZendeskClientFactory clientFactory)
         {
+            _output = output;
             _clientFactory = clientFactory;
         }
 
         [Fact]
         public async Task CreateOrganisation()
         {
+            var settings = new ZendeskSettings();
+
+            _output.WriteLine($"url: {settings.Url}");
+
             var client = _clientFactory.GetClient();
 
             var id = Guid.NewGuid().ToString();
