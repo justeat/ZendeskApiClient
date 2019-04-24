e09d68cc5a0ad98b1964bd1b98ba5fb7b3721211
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-04-24 15:13:16 +0100

    refactor ticket comments resource to use new conventions

--- src/ZendeskApi.Client/Resources/Ticket/TicketCommentsResource.cs
+++ src/ZendeskApi.Client/Resources/Ticket/TicketCommentsResource.cs
@@ -1,55 +1,49 @@
-using System;
 using System.Threading.Tasks;
-using System.Net.Http;
 using Microsoft.Extensions.Logging;
-using ZendeskApi.Client.Extensions;
 using ZendeskApi.Client.Models;
 using ZendeskApi.Client.Requests;
 using ZendeskApi.Client.Responses;
 
 namespace ZendeskApi.Client.Resources
 {
-    public class TicketCommentsResource : ITicketCommentsResource
+    public class TicketCommentsResource : AbstractBaseResource<TicketCommentsResource>, 
+        ITicketCommentsResource
     {
         private const string ResourceUri = "api/v2/tickets/{0}/comments";
 
-        private readonly IZendeskApiClient _apiClient;
-        private readonly ILogger _logger;
-
-        private readonly Func<ILogger, string, IDisposable> _loggerScope = 
-            LoggerMessage.DefineScope<string>(typeof(TicketCommentsResource).Name + ": {0}");
-
         private readonly ITicketsResource _ticketsResource;
 
-        public TicketCommentsResource(IZendeskApiClient apiClient, ILogger logger)
+        public TicketCommentsResource(
+            IZendeskApiClient apiClient, 
+            ILogger logger)
+            : base(apiClient, logger, "ticket_comments")
         {
-            _apiClient = apiClient;
-            _logger = logger;
-
             _ticketsResource = new TicketsResource(apiClient, logger);
         }
 
-        public async Task<TicketCommentsListResponse> ListAsync(long ticketId, PagerParameters pager = null)
+        public async Task<TicketCommentsListResponse> ListAsync(
+            long ticketId, 
+            PagerParameters pager = null)
         {
-            using (_loggerScope(_logger, $"ListAsync({ticketId})"))
-            using (var client = _apiClient.CreateClient())
-            {
-                var response = await client.GetAsync(string.Format(ResourceUri, ticketId), pager).ConfigureAwait(false);
-
-                await response.ThrowIfUnsuccessful("ticket_comments#list-comments");
-
-                return await  response.Content.ReadAsAsync<TicketCommentsListResponse>();
-            }
+            return await GetAsync<TicketCommentsListResponse>(
+                string.Format(ResourceUri, ticketId),
+                "list-comments",
+                $"ListAsync({ticketId})",
+                pager);
         }
 
-        public async Task AddComment(long ticketId, TicketComment ticketComment)
+        public async Task AddComment(
+            long ticketId, 
+            TicketComment ticketComment)
         {
             var ticket = new TicketUpdateRequest(ticketId)
             {
                 Comment = ticketComment
             };
 
-            await _ticketsResource.UpdateAsync(ticket).ConfigureAwait(false);
+            await _ticketsResource
+                .UpdateAsync(ticket)
+                .ConfigureAwait(false);
         }
     }
 }
