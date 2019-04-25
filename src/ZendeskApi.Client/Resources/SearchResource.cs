b39d4afa3c718e96ba1bc9fa142cc84e6641d46d
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-04-25 16:05:58 +0100

    refactor search resource to use new conventions

--- src/ZendeskApi.Client/Resources/SearchResource.cs
+++ src/ZendeskApi.Client/Resources/SearchResource.cs
@@ -1,67 +1,59 @@
 using System;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using ZendeskApi.Client.Converters;
-using ZendeskApi.Client.Extensions;
 using ZendeskApi.Client.Models;
 using ZendeskApi.Client.Queries;
 using ZendeskApi.Client.Responses;
 
 namespace ZendeskApi.Client.Resources
 {
-    public class SearchResource : ISearchResource
+    public class SearchResource : AbstractBaseResource<SearchResource>,
+        ISearchResource
     {
         private const string SearchUri = "api/v2/search";
 
-        private readonly IZendeskApiClient _apiClient;
-        private readonly ILogger _logger;
+        public SearchResource(
+            IZendeskApiClient apiClient, 
+            ILogger logger)
+            : base(apiClient, logger, "search")
+        { }
 
-        private readonly Func<ILogger, string, IDisposable> _loggerScope = LoggerMessage.DefineScope<string>(typeof(SearchResource).Name + ": {0}");
-
-        public SearchResource(IZendeskApiClient apiClient, ILogger logger)
-        {
-            _apiClient = apiClient;
-            _logger = logger;
-        }
-
-        public async Task<SearchResponse<ISearchResult>> SearchAsync(Action<IZendeskQuery> builder,
-            PagerParameters pager = null, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<SearchResponse<ISearchResult>> SearchAsync(
+            Action<IZendeskQuery> builder,
+            PagerParameters pager = null, 
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             var query = new ZendeskQuery();
+
             builder(query);
 
-            using (_loggerScope(_logger, "SearchAsync"))
-            using (var client = _apiClient.CreateClient())
-            {
-                var response = await client.GetAsync($"{SearchUri}?{query.BuildQuery()}", pager, cancellationToken)
-                    .ConfigureAwait(false);
-
-                await response.ThrowIfUnsuccessful("search#list-search-results");
-
-                return await response.Content.ReadAsAsync<SearchResponse<ISearchResult>>(new SearchJsonConverter());
-            }
+            return await GetAsync<SearchResponse<ISearchResult>>(
+                $"{SearchUri}?{query.BuildQuery()}",
+                "list-search-results",
+                "SearchAsync",
+                pager,
+                new SearchJsonConverter());
         }
 
-        public async Task<SearchResponse<T>> SearchAsync<T>(Action<IZendeskQuery> builder, PagerParameters pager = null,
-            CancellationToken cancellationToken = default(CancellationToken)) where T : ISearchResult
+        public async Task<SearchResponse<T>> SearchAsync<T>(
+            Action<IZendeskQuery> builder, 
+            PagerParameters pager = null,
+            CancellationToken cancellationToken = default(CancellationToken)) 
+            where T : ISearchResult
         {
             var query = new ZendeskQuery();
+
             builder(query);
 
             query.WithTypeFilter<T>();
 
-            using (_loggerScope(_logger, "SearchAsync"))
-            using (var client = _apiClient.CreateClient())
-            {
-                var response = await client.GetAsync($"{SearchUri}?{query.BuildQuery()}", pager, cancellationToken)
-                    .ConfigureAwait(false);
-
-                await response.ThrowIfUnsuccessful("search#list-search-results");
-
-                return await response.Content.ReadAsAsync<SearchResponse<T>>();
-            }
+            return await GetAsync<SearchResponse<T>>(
+                $"{SearchUri}?{query.BuildQuery()}",
+                "list-search-results",
+                "SearchAsync",
+                pager);
         }
     }
 }
