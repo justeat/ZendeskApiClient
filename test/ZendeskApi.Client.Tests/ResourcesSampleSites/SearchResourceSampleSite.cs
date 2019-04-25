2813acd7088937b4770e34634995cd6ebaa27424
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-04-25 16:06:07 +0100

    add search resource tests

--- test/ZendeskApi.Client.Tests/ResourcesSampleSites/SearchResourceSampleSite.cs
+++ test/ZendeskApi.Client.Tests/ResourcesSampleSites/SearchResourceSampleSite.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Routing;
 using ZendeskApi.Client.Models;
 using ZendeskApi.Client.Responses;
@@ -21,11 +22,12 @@ namespace ZendeskApi.Client.Tests.ResourcesSampleSites
                 return rb => rb
                     .MapGet("api/v2/search", (req, resp, routeData) =>
                     {
-                        var obj = new ISearchResult[] {
+                        var items = new ISearchResult[] {
                             new Ticket { Id = 1, Url = new Uri("https://company.zendesk.com/api/v2/tickets/1.json") },
                             new Group { Id = 2, Url = new Uri("https://company.zendesk.com/api/v2/groups/2.json") },
                             new Organization { Id = 3, Url = new Uri("https://company.zendesk.com/api/v2/organizations/3.json") },
-                            new UserResponse { Id = 4, Url = new Uri("https://company.zendesk.com/api/v2/users/4.json") }
+                            new UserResponse { Id = 4, Url = new Uri("https://company.zendesk.com/api/v2/users/4.json") },
+                            new Ticket { Id = 5, Url = new Uri("https://company.zendesk.com/api/v2/tickets/5.json") }
                         };
 
                         if (req.Query.ContainsKey("query") && !string.IsNullOrEmpty(req.Query["query"][0])) {
@@ -33,15 +35,33 @@ namespace ZendeskApi.Client.Tests.ResourcesSampleSites
 
                             if (query[1] == "ticket")
                             {
-                                obj = obj.OfType<Ticket>().ToArray();
+                                items = items.OfType<Ticket>().ToArray();
                             }
                         }
 
+                        if (req.Query.ContainsKey("page") &&
+                            req.Query.ContainsKey("per_page") &&
+                            int.TryParse(req.Query["page"].ToString(), out var page) &&
+                            int.TryParse(req.Query["per_page"].ToString(), out var size))
+                        {
+                            if (page == int.MaxValue && size == int.MaxValue)
+                            {
+                                resp.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+                                return Task.FromResult(resp);
+                            }
+
+                            items = items
+                                .Skip((page - 1) * size)
+                                .Take(size)
+                                .ToArray();
+                        }
+
                         resp.StatusCode = (int)HttpStatusCode.OK;
+
                         return resp.WriteAsJson(new SearchResponse<ISearchResult>
                         {
-                            Results = obj,
-                            Count = obj.Length,
+                            Results = items,
+                            Count = items.Length,
                             NextPage = new Uri("https://foo.zendesk.com/api/v2/search.json?query=\"type:GroupResponse hello\"&sort_by=created_at&sort_order=desc&page=2")
                         });
                     });
