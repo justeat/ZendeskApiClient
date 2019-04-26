55df4e19629d395e0f2626aae9fc5a92d516c4d7
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-04-26 07:39:28 +0100

    add cancellation tokens to user identities

--- src/ZendeskApi.Client/Resources/Interfaces/User/IUserIdentityResource.cs
+++ src/ZendeskApi.Client/Resources/Interfaces/User/IUserIdentityResource.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using ZendeskApi.Client.Models;
 using ZendeskApi.Client.Responses;
@@ -6,11 +7,33 @@ namespace ZendeskApi.Client.Resources
 {
     public interface IUserIdentityResource
     {
-        Task<IPagination<UserIdentity>> GetAllForUserAsync(long userId, PagerParameters pager = null);
-        Task<UserIdentity> GetIdentityForUserAsync(long userId, long identityId);
-        Task<UserIdentity> CreateUserIdentityAsync(UserIdentity identity, long userId);
-        Task<UserIdentity> CreateEndUserIdentityAsync(UserIdentity identity, long endUserId);
-        Task<UserIdentity> UpdateAsync(UserIdentity identity);
-        Task DeleteAsync(long userId, long identityId);
+        Task<IPagination<UserIdentity>> GetAllForUserAsync(
+            long userId, 
+            PagerParameters pager = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserIdentity> GetIdentityForUserAsync(
+            long userId, 
+            long identityId,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserIdentity> CreateUserIdentityAsync(
+            UserIdentity identity, 
+            long userId,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserIdentity> CreateEndUserIdentityAsync(
+            UserIdentity identity, 
+            long endUserId,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserIdentity> UpdateAsync(
+            UserIdentity identity,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task DeleteAsync(
+            long userId, 
+            long identityId,
+            CancellationToken cancellationToken = default(CancellationToken));
     }
 }
