18dd3e7f6d99372b0695c4df522e28432e0d3347
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-04-26 07:36:57 +0100

    add cancellation tokens to user fields

--- src/ZendeskApi.Client/Resources/Interfaces/User/IUserFieldsResource.cs
+++ src/ZendeskApi.Client/Resources/Interfaces/User/IUserFieldsResource.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
 using ZendeskApi.Client.Models;
 using ZendeskApi.Client.Responses;
 
@@ -6,10 +7,24 @@ namespace ZendeskApi.Client.Resources
 {
     public interface IUserFieldsResource
     {
-        Task<IPagination<UserField>> GetAllAsync(PagerParameters pager = null);
-        Task<UserField> GetAsync(long userFieldId);
-        Task<UserField> CreateAsync(UserField userField);
-        Task<UserField> UpdateAsync(UserField userField);
-        Task DeleteAsync(long userFieldId);
+        Task<IPagination<UserField>> GetAllAsync(
+            PagerParameters pager = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserField> GetAsync(
+            long userFieldId,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserField> CreateAsync(
+            UserField userField,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<UserField> UpdateAsync(
+            UserField userField,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task DeleteAsync(
+            long userFieldId,
+            CancellationToken cancellationToken = default(CancellationToken));
     }
 }
--- src/ZendeskApi.Client/Resources/User/UserFieldsResource.cs
+++ src/ZendeskApi.Client/Resources/User/UserFieldsResource.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using ZendeskApi.Client.Models;
@@ -17,56 +18,71 @@ namespace ZendeskApi.Client.Resources
             : base(apiClient, logger, "user_fields")
         { }
 
-        public async Task<IPagination<UserField>> GetAllAsync(PagerParameters pager = null)
+        public async Task<IPagination<UserField>> GetAllAsync(
+            PagerParameters pager = null,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             return await GetAsync<UserFieldsResponse>(
                 ResourceUri,
                 "list-user-fields",
                 "GetAllAsync",
-                pager);
+                pager,
+                cancellationToken: cancellationToken);
         }
 
-        public async Task<UserField> GetAsync(long userFieldId)
+        public async Task<UserField> GetAsync(
+            long userFieldId,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             var response = await GetWithNotFoundCheckAsync<UserFieldResponse>(
                 $"{ResourceUri}/{userFieldId}",
                 "show-user-field",
                 $"GetAsync({userFieldId})",
-                $"UserResponse Field {userFieldId} not found");
+                $"UserResponse Field {userFieldId} not found",
+                cancellationToken: cancellationToken);
 
             return response?
                 .UserField;
         }
 
-        public async Task<UserField> CreateAsync(UserField userField)
+        public async Task<UserField> CreateAsync(
+            UserField userField,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             var response = await CreateAsync<UserFieldResponse, UserFieldCreateUpdateRequest>(
                 ResourceUri,
                 new UserFieldCreateUpdateRequest(userField),
-                "create-user-field");
+                "create-user-field",
+                cancellationToken: cancellationToken);
 
             return response?
                 .UserField;
         }
 
-        public async Task<UserField> UpdateAsync(UserField userField)
+        public async Task<UserField> UpdateAsync(
+            UserField userField,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             var response = await UpdateWithNotFoundCheckAsync<UserFieldResponse, UserFieldCreateUpdateRequest>(
                 $"{ResourceUri}/{userField.Id}",
                 new UserFieldCreateUpdateRequest(userField),
                 "update-user-field",
-                $"Cannot update user field as user field {userField.Id} cannot be found");
+                $"Cannot update user field as user field {userField.Id} cannot be found",
+                cancellationToken: cancellationToken);
 
             return response?
                 .UserField;
         }
 
-        public async Task DeleteAsync(long userFieldId)
+        public async Task DeleteAsync(
+            long userFieldId,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             await DeleteAsync(
                 ResourceUri,
                 userFieldId,
-                "delete-user-field");
+                "delete-user-field",
+                cancellationToken: cancellationToken);
         }
     }
 }
