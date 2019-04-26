193b2184b1b61d0496ddf64ff9835745f180c9cf
Jonathan Stowell <jonathan.stowell@just-eat.com>
2019-04-26 07:35:07 +0100

    add cancellation tokens to deleted users

--- src/ZendeskApi.Client/Resources/Interfaces/User/IDeletedUsersResource.cs
+++ src/ZendeskApi.Client/Resources/Interfaces/User/IDeletedUsersResource.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using ZendeskApi.Client.Models;
 using ZendeskApi.Client.Requests;
@@ -9,15 +10,21 @@ namespace ZendeskApi.Client.Resources
     public interface IDeletedUsersResource
     {
         #region List Deleted Users
-        Task<UsersListResponse> ListAsync(PagerParameters pager = null);
+        Task<UsersListResponse> ListAsync(
+            PagerParameters pager = null,
+            CancellationToken cancellationToken = default(CancellationToken));
         #endregion
 
         #region Get Deleted Users
-        Task<UserResponse> GetAsync(long userId);
+        Task<UserResponse> GetAsync(
+            long userId,
+            CancellationToken cancellationToken = default(CancellationToken));
         #endregion
 
         #region Permanently Delete Users
-        Task PermanentlyDeleteAsync(long userId);
+        Task PermanentlyDeleteAsync(
+            long userId,
+            CancellationToken cancellationToken = default(CancellationToken));
         #endregion
     }
 }
