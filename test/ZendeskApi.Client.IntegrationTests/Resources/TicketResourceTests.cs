0bedb9d63f3d338e71f950c650f74c7cc6b6bfd5
James Russell <james.russell@justeattakeaway.com>
2021-08-17 17:26:09 +0100

    Add cursor pagination tests: Ticket Fields

---
+++ test/ZendeskApi.Client.IntegrationTests/Resources/TicketResourceTests.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using ZendeskApi.Client.IntegrationTests.Factories;
+using ZendeskApi.Client.Models;
+
+namespace ZendeskApi.Client.IntegrationTests.Resources
+{
+    public class TicketResourceTests : IClassFixture<ZendeskClientFactory>
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly ZendeskClientFactory _clientFactory;
+
+        public TicketResourceTests(
+            ITestOutputHelper output,
+            ZendeskClientFactory clientFactory)
+        {
+            _output = output;
+            _clientFactory = clientFactory;
+        }
+
+
+        [Fact]
+        public async Task GetAllAsync_WhenCalledWithCursorPagination_ShouldReturnTickets()
+        {
+            var client = _clientFactory.GetClient();
+
+            var results = await client
+                .Tickets.GetAllAsync(new CursorPager());
+             
+            Assert.NotNull(results);
+            Assert.Equal(100, results.Count());
+        }
+    }
+}
