8a80d3326e367d5891e083a60769ed120c130569
James Russell <james.russell@justeattakeaway.com>
2021-08-24 16:50:39 +0100

    Add SatisfactionRating Resource integration test

--- test/ZendeskApi.Client.IntegrationTests/Resources/UsersResourceTests.cs
+++ test/ZendeskApi.Client.IntegrationTests/Resources/UsersResourceTests.cs
@@ -5,45 +5,23 @@ using ZendeskApi.Client.Models;
 
 namespace ZendeskApi.Client.IntegrationTests.Resources
 {
-    public class UsersResourceTests : IClassFixture<ZendeskClientFactory>
+    public class SatisfactionRatingResourceTests : IClassFixture<ZendeskClientFactory>
     {
         private readonly ZendeskClientFactory _clientFactory;
 
-        public UsersResourceTests(
+        public SatisfactionRatingResourceTests(
             ZendeskClientFactory clientFactory)
         {
             _clientFactory = clientFactory;
         }
         
         [Fact]
-        public async Task GetAllAsync_WhenCalledWithCursorPagination_ShouldReturnUsers()
+        public async Task GetAllAsync_WhenCalledWithCursorPagination_ShouldReturnSatisfactionRatings()
         {
             var client = _clientFactory.GetClient();
 
             var results = await client
-                .Users.GetAllAsync(new CursorPager());
-
-            Assert.NotNull(results);
-        }
-
-        [Fact]
-        public async Task GetAllByGroupIdAsync_WhenCalledWithCursorPagination_ShouldReturnUsers()
-        {
-            var client = _clientFactory.GetClient();
-
-            var results = await client
-                .Users.GetAllByGroupIdAsync(28963465, new CursorPager());
-
-            Assert.NotNull(results);
-        }
-
-        [Fact]
-        public async Task GetAllByOrganizationIdAsync_WhenCalledWithCursorPagination_ShouldReturnUsers()
-        {
-            var client = _clientFactory.GetClient();
-
-            var results = await client
-                .Users.GetAllByOrganizationIdAsync(360106870118, new CursorPager());
+                .SatisfactionRatings.GetAllAsync(new CursorPager());
 
             Assert.NotNull(results);
         }
